21ee97eaaa825831f62dc9922c66f5f84e44d47e
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2015-07-09 13:05:32 +0530

    [SUBMIT] final changes made

---
+++ soil.js
@@ -0,0 +1,28 @@
+function f1()
+{
+	document.getElementById("Imagine").style.backgroundImage="url"+"("+"'Change.jpg'"+")";
+	document.getElementById("Create").innerHTML='';
+	document.getElementById("Create").innerHTML="<pre><p id='f1'>Hello  there!  I  am  Vijay.<br/>I  am  an  Electrical  and  Electronics  Engineer  studying  at  NITT.<br/>I  am  interested  in  Programming  and  Designing, <br/>Because  I  love  to  Imagine  and  Create.</p></pre>";
+	document.getElementById("Imagine").style.height="45%";
+}
+function f2()
+{
+    document.getElementById("Imagine").style.backgroundImage="url"+"("+"'Imagine.jpg'"+")";
+	document.getElementById("Create").innerHTML='';
+	document.getElementById("Create").innerHTML="Imagine and Create."
+	document.getElementById("Imagine").style.height="30%";
+}
+function f3()
+{
+	document.getElementById("Explore").style.backgroundImage="url"+"("+"'Change.jpg'"+")";
+	document.getElementById("Innovate").innerHTML='';
+	document.getElementById("Innovate").innerHTML="<pre><p id='f2'>I like Gaming,Reading fiction and Movies.<br/>I am naturally Inquisitive and learn quickly.<br/>I like to Explore and Innovate.</p></pre>";
+	document.getElementById("Explore").style.height="45%";
+}
+function f4()
+{
+    document.getElementById("Explore").style.backgroundImage="url"+"("+"'Explore.jpg'"+")";
+	document.getElementById("Innovate").innerHTML='';
+	document.getElementById("Innovate").innerHTML="Explore and Innovate."
+	document.getElementById("Explore").style.height="30%";
+}
