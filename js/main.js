2f22ee2283e95d54253c826fc02a1eb733c4a4ef
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-22 00:20:31 +0530

    add links to spider.nitt.edu

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $('#scroll-door').mouseover(function(){
 $('#scroll-door').mouseout(function(){
   $('#scroll-door').attr({"src":"images/door_close.png"});
 });
+$('#scroll-door').click(function(event) {
+  event.preventDefault();
+  console.log(this);
+  var link = this;
+  $.smoothScroll({
+  scrollTarget: link.getAttribute("tolink"),
+  speed: 1500,
+  easing: "swing"
+  });
+});
