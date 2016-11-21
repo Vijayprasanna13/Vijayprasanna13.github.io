47fcc3d4731a7d36e0a930142fe35f3caf8313af
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-21 23:08:51 +0530

    Restucture the whole damn thing

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('#scroll-door').mouseover(function(){
+  $('#scroll-door').attr({"src":"images/door_open.png"});
+});
+$('#scroll-door').mouseout(function(){
+  $('#scroll-door').attr({"src":"images/door_close.png"});
+});
