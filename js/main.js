505486a695b7acb77354fa69c600a43f5c6706e3
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-22 23:07:26 +0530

    Add background image Add border to door logo Replace door icon with size compatible one

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ $('#scroll-door').mouseover(function(){
 $('#scroll-door').mouseout(function(){
   $('#scroll-door').attr({"src":"images/door_close.png"});
 });
-
+var setlang = 0;
 $('#scroll-door').click(function(event) {
+
   event.preventDefault();
   console.log(this);
   var link = this;
@@ -14,12 +15,19 @@ $('#scroll-door').click(function(event) {
   speed: 1500,
   easing: "swing"
   });
+  if(!setlang){
+      lang_wrapper();
+      setlang = 1;
+  }
+});
+
+function lang_wrapper(){
   var images = ["html","css","php","bootstrap","jquery","qt","laravel","javascript","python","react","lumen","octave","milligram","matlab","babel","omnet","git"];
   $.map(images,function(data,i){
-      $("#gallery").append("<div class='lang-elem'><a><img src='images/langs/"+data+".png'></a></div>");
-  });  
+      $("#gallery").append("<div class='lang-elem'><a><img id='lang"+i+"' src='images/langs/"+data+".png'></a></div>");
+  });
   $("#gallery").justifiedGallery();
-});
+}
 
 $('#down').click(function(event) {
   event.preventDefault();
