e87b37c92f63ccb11efcadc658713107fa9dd230
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2016-11-22 19:23:01 +0530

    Modify line hieght; Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $('#scroll-door').mouseover(function(){
 $('#scroll-door').mouseout(function(){
   $('#scroll-door').attr({"src":"images/door_close.png"});
 });
+
 $('#scroll-door').click(function(event) {
   event.preventDefault();
   console.log(this);
@@ -13,12 +14,23 @@ $('#scroll-door').click(function(event) {
   speed: 1500,
   easing: "swing"
   });
-
 });
+
 var images = ["html","css","php","bootstrap","jquery","qt","laravel","js","python","react","lumen","octave","milligram","matlab","babel","omnet","git"];
 $(document).ready(function(){
   $.map(images,function(data,i){
       $("#gallery").append("<div class='lang-elem'><a><img src='images/langs/"+data+".png'></a></div>");
   });
   $("#gallery").justifiedGallery();
+});
+
+$('#down').click(function(event) {
+  event.preventDefault();
+  console.log(this);
+  var link = this;
+  $.smoothScroll({
+  scrollTarget: link.getAttribute("tolink"),
+  speed: 1500,
+  easing: "swing"
+  });
 });
